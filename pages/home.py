76afe8807cee9da2d1ae8edd0376cd9f51684d01
suramyavns <sdidwania645@gmail.com>
2024-07-14 14:51:25 +0530

    auth

--- pages/home.py
+++ pages/home.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from streamlit_card import card
+from libs.db import Blogs,Users
 st.set_page_config(page_title='BlogBook',initial_sidebar_state='collapsed')
 st.markdown(
     """
@@ -13,10 +14,19 @@ st.markdown(
 )
 from streamlit_card import card
 from st_pages import hide_pages
-hide_pages(['main'])
+def logout():
+    if 'user' in st.session_state:
+        del st.session_state.user
+    print(st.session_state)
+    st.session_state['users']=Users()
+    st.session_state['blogs']=Blogs()
+hide_pages(['main','login','create'])
 st.title('Home')
 if st.button('Write a blog!'):
     st.switch_page('pages/create.py')
+if st.button('Sign Out'):
+    logout()
+    st.switch_page('pages/login.py')
 if 'blogs' in st.session_state:
     blogdb = st.session_state.blogs
     blogs = blogdb.readall()
--- pages/login.py
+++ pages/login.py
@@ -1,9 +1,22 @@
 import streamlit as st
 from st_pages import hide_pages
-from libs.db import Users
+from libs.db import Users,Blogs
 st.set_page_config(page_title='Login',initial_sidebar_state='collapsed')
-hide_pages(['main'])
 st.title('Sign In')
+if 'user' in st.session_state:
+    hide_pages(['main'])
+else:
+    hide_pages(['main','login','home','create'])
+    st.markdown(
+        """
+    <style>
+        [data-testid="collapsedControl"] {
+            display: none
+        }
+    </style>
+    """,
+        unsafe_allow_html=True,
+    )
 def loginUser():
     uname = st.session_state.username
     passwd = st.session_state.passwd
@@ -13,10 +26,13 @@ def loginUser():
             st.error('Invalid Credentials')
         else:
             st.session_state['user'] = userdb.getUser(uname,passwd)
-            st.switch_page('main.py')
+            if 'blogs' not in st.session_state:
+                st.session_state['blogs'] = Blogs()
+            return True
     else:
         st.error('Please fill all the fields.')
 with st.form('user-login',clear_on_submit=True,border=True):
     username = st.text_input('Username',key='username',placeholder='John Doe')
     password = st.text_input('Password',type='password',key='passwd',placeholder='********')
-    st.form_submit_button('Sign-In',on_click=loginUser)
+    if st.form_submit_button('Sign-In',on_click=loginUser):
+        st.switch_page('main.py')
