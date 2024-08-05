fd7d3407b298264cde7179db50b6a0e08860369c
suramyavns <sdidwania645@gmail.com>
2024-08-05 17:43:41 +0530

    refine

--- BlogBook.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-st.title('BlogBook')
-st.image('./assets/images/undraw_Welcome_re_h3d9.png',width=300)
-st.header('Coming soon...')
-st.subheader("Blogging has never got easier but guess what's better...socialising with other bloggers!")
-st.button('Get Started')
